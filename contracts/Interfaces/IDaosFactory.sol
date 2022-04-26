3660ca6370cee7609b46cba8c548063aae7834e5
MouradM <wassakh@gmail.com>
2022-04-26 18:19:42 +0200

    delete simple storage

--- contracts/Interfaces/IDaosFactory.sol
+++ contracts/Interfaces/IDaosFactory.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.13;
 interface IDaosFactory {
     
     event DaoCreated(address indexed _address);
-    function getDao(address tokenA, address tokenB) external view returns (address pair);
+    function getDao(address _address) external view returns (address pair);
     function getDaos(uint) external view returns (address pair);
-    function createDao(address _address, address tokenB) external returns (address pair);
+    function createDao(address _address) external returns (address pair);
 }
