c2fbac4655a9b0151287a70212260c92c51e581b
Jeremie Lucotte <lucottejeremie@gmail.com>
2022-05-14 23:07:14 +0200

    fix contracts

--- contracts/DaosFactory.sol
+++ contracts/DaosFactory.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-import './interfaces/IDaosFactory.sol';
+// import './interfaces/IDaosFactory.sol';
 import './DaoBase.sol';
 import './Modules/Implementations/MembershipDao.sol';
 
